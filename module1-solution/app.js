49d00990fa847695117d1993accfc24ca50b3679
ReneOhayon <rene.ohayon@gmail.com>
2016-10-15 01:41:23 +0200

    Final submission

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,28 +1,39 @@
 (function () {
 'use strict';
 
-angular.module('NameCalculator', [])
+angular.module('LunchItemCounter', [])
 
-.controller('NameCaculatorController', function ($scope) {
-  $scope.name = "";
-  $scope.totalValue = 0;
+.controller('LunchItemCounterController', LunchItemCounterController);
 
-  $scope.displayNumeric = function () {
-    var totalNameValue = calculatNumericForString($scope.name);
-    $scope.totalValue = totalNameValue;
+LunchItemCounterController.$inject= ['$scope'];
+
+function LunchItemCounterController($scope){
+  $scope.lunchItems = "";
+  $scope.message = "";
+
+  $scope.countItems = function () {
+    var str = $scope.lunchItems;
+    var arrayOfStrings = str.split(",");
+    var len = getCount(arrayOfStrings);
+
+    if(len==0){
+      $scope.message = "Please enter data first";
+    }else if (len > 3){
+      $scope.message = "Too Much!!";
+    }else{
+      $scope.message = "Enjoy!!";
+    }
   };
 
-
-  function calculatNumericForString(string) {
-    var totalStringValue = 0;
-    for (var i = 0; i < string.length; i++) {
-      totalStringValue += string.charCodeAt(i);
+  // Checking if the lunch box countains any empty item
+  function getCount(array) {
+    var count = 0;
+    for(var i = 0;i<array.length;++i){
+      if(array[i].trim().length>0)
+        count+=1;
     }
-
-    return totalStringValue;
+    return count;
   }
-
-});
-
+}
 
 })();
